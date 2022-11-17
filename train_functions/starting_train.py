a8e990da3a5e7f89e26a13958eba71bc35439068
ashraj <ashraj@umich.edu>
2022-11-16 18:20:58 -0800

    Fixed loop hang bugs, added accuracy stuff in train loop

--- train_functions/starting_train.py
+++ train_functions/starting_train.py
@@ -35,12 +35,14 @@ def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval):
     
     model.train()
     train_losses=[]
+    train_accuracies=[]
     step = 1
     for epoch in range(epochs):
         #print(f"Epoch {epoch + 1} of {epochs}")
 
         progress_bar = tqdm(train_loader, leave=False)
         losses = []
+        accuracies = []
         total = 0
         for inputs, target in progress_bar:
             model.zero_grad() # reset the gradient values from previous iteration
@@ -59,24 +61,26 @@ def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval):
             #print("hello2")
             optimizer.step() # update weights: using gradients to update weights 
             #print("hello3")
-            progress_bar.set_description(f'Loss: {loss.item():.3f}') 
             #print("hello4")
             losses.append(loss.item()) # keep track of losses as we iterate 
+            accuracy = compute_accuracy(output.squeeze(),target.float())
+            accuracies.append(accuracy)
+            progress_bar.set_description(f'Loss: {loss.item():.3f} Accuracy: {accuracy:.3f}') 
             total += 1
             # Periodically evaluate our model + log to Tensorboard
             if step % n_eval == 0:
                 # Compute training loss and accuracy.
                 #print("hello5")
-                train_loss,train_accuracy = evaluate(train_loader,model,loss_fn)
+                #train_loss,train_accuracy = evaluate(train_loader,model,loss_fn)
                 #print("hello6")
                 # Compute validation loss and accuracy.
                 # Log the results to Tensorboard. 
                 # Don't forget to turn off gradient calculations!
                 val_loss,val_accuracy = evaluate(val_loader, model, loss_fn)
                 #print("hello7")
-                writer.add_scalar('Loss/train', train_loss, step//n_eval)
+                #writer.add_scalar('Loss/train', train_loss, step//n_eval)
                 writer.add_scalar('Loss/validation', val_accuracy, step//n_eval)
-                writer.add_scalar('Accuracy/train', train_accuracy, step//n_eval)
+                #writer.add_scalar('Accuracy/train', train_accuracy, step//n_eval)
                 writer.add_scalar('Accuracy/validation', val_accuracy, step//n_eval)
                 #print("hello8")
                 model.train()
@@ -84,9 +88,11 @@ def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval):
             step += 1
 
         epoch_loss = sum(losses) / total #calculate an overall loss for an epoch 
+        epoch_accuracy = sum(accuracy)/total
+        train_accuracies.append(epoch_accuracy)
         train_losses.append(epoch_loss)
 
-        tqdm.write(f'Epoch #{epoch + 1}\tTrain Loss: {epoch_loss:.3f}')
+        tqdm.write(f'Epoch #{epoch + 1}\tTrain Loss: {epoch_loss:.3f}\tTrain Accuracy: {epoch_accuracy:.3f}')
     print(evaluate(val_loader,model,loss_fn))
 
 def compute_accuracy(outputs, labels):
